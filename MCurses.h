9712b1c9b8b524a19997216ca758e70677fc5c71
chagasbezerra091-code <chagasbezerra091@gmail.com>
2025-12-09 23:35:58 -0300

    Create MCurses.h

---
+++ MCurses.h
@@ -0,0 +1,26 @@
+#include <Colors.h> /** */
+#include <stdio.h>
+#define COR_VERMELHO 1
+#define COR_VERDE    2
+#define COR_AZUL     3
+#define COR_CIANO         4
+#define COR_PRETO         5
+#define COR_RESET         6
+#define COR_BRANCO         7
+#define COR_AMARELO         8
+#define COR_TRANSPARENTE         9
+#define COR_LARANJA         10
+typedef struct {
+ITALIC,
+BOLD,
+SANS_SERIF,
+MONOSPACE
+}
+enum window {
+GRANDE = 1,
+MEDIA = 2,
+PEQUENA = 3
+}
+printf("Janelas: %d \
+        Logs: %d    \
+\n");
