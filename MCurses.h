e5b4269f9128ff689e9cb1788de060bafc7133cf
chagasbezerra091-code <chagasbezerra091@gmail.com>
2025-12-09 23:52:03 -0300

    Update MCurses.h

--- MCurses.h
+++ MCurses.h
@@ -1,27 +1,63 @@
-/////////ARQUIVO: MCurses.h \\\\\\\\\\
-#include <Colors.h> /** https://github.com/chagasbezerra091-code/MyLibs/blob/main/Colors.h */
+#ifndef MCURSES_H
+#define MCURSES_H
+
 #include <stdio.h>
-#define COR_VERMELHO 1
-#define COR_VERDE    2
-#define COR_AZUL     3
-#define COR_CIANO         4
-#define COR_PRETO         5
-#define COR_RESET         6
-#define COR_BRANCO         7
-#define COR_AMARELO         8
-#define COR_TRANSPARENTE         9
-#define COR_LARANJA         10
+#include "Colors.h"
+
+// ---- Cores (enum estilo ncurses) ----
+enum MCursesColors {
+    COR_VERMELHO = 1,
+    COR_VERDE,
+    COR_AZUL,
+    COR_CIANO,
+    COR_PRETO,
+    COR_RESET,
+    COR_BRANCO,
+    COR_AMARELO,
+    COR_TRANSPARENTE,
+    COR_LARANJA
+};
+
+// ---- Estilo de texto ----
+enum MCursesStyle {
+    ESTILO_NORMAL = 0,
+    ESTILO_BOLD   = 1 << 0,
+    ESTILO_ITALIC = 1 << 1,
+    ESTILO_UNDER  = 1 << 2,
+    ESTILO_REVERSE= 1 << 3
+};
+
+// ---- Tamanho das janelas ----
+enum MCursesWindowSize {
+    JANELA_GRANDE  = 1,
+    JANELA_MEDIA   = 2,
+    JANELA_PEQUENA = 3
+};
+
+// ---- Estrutura de janela (tipo WINDOW do ncurses) ----
 typedef struct {
-ITALIC,
-BOLD,
-SANS_SERIF,
-MONOSPACE
+    int x;
+    int y;
+    int w;
+    int h;
+    int cor;
+    int estilo;
+} MCURSES_WINDOW;
+
+#endif
+#define MCURSES_MASK1 (1 << 0) // 00000001
+#define MCURSES_MASK2 (1 << 1) // 00000010
+#define MCURSES_MASK3 (1 << 2) // 00000100
+#define MCURSES_MASK4 (1 << 3) // 00001000
+#define MCURSES_MASK5 (1 << 4) // 00010000
+#define MCURSES_MASK6 (1 << 5) // 00100000
+#define MCURSES_MASK7 (1 << 6) // 01000000
+#define MCURSES_MASK8 (1 << 7) // 10000000
+#define MCURSES_MASK9 (1 << 8) // 1 00000000
+int flags = MCURSES_MASK1 | MCURSES_MASK9;
+typedef struct {
+int btn_ui;
+int btn_style;
+int btn_onclick;
+int btn_color;
 }
-enum window {
-GRANDE = 1,
-MEDIA = 2,
-PEQUENA = 3
-}
-printf("Janelas: %d \
-        Logs: %d    \
-\n");
